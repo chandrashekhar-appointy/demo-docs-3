ca699043fd993ae8c737d099825038d17e4cf44b
chandrashekhar-appointy <chandrashekhar@appointy.com>
2025-07-04 13:06:24 +0530

    hellooo

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const withNextra = nextra({
 
 export default withNextra({
 	// ... Other Next.js config options
-	output: 'export',
+	// output: 'export',
 	distDir: 'dist',
 	trailingSlash: true,
 	images: {
